923d0300cb40aa9fedf15f2d44f16821aea3a6d2
Marcel Suter <marcel.suter@bzz.ch>
2024-10-01 07:21:37 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# M320 / LB01 / Aufgabe 1 (Version 2024)
+TODO write your name here
+```
---
+++ main_test.py
@@ -0,0 +1,4 @@
+""" This file tests if Pytest is working correctly """
+
+def test(capsys):
+    assert True
